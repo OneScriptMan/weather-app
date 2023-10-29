7931aedb23835beb2e5140333446f10d0f1550ea
OneScriptMan <vladzam79@gmail.com>
2023-10-29 19:18:56 +0300

    add render date and season picture

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ async function getWeather() {
     `https://api.openweathermap.org/data/2.5/weather?lat=51.50&lon=-0.127&appid=${apiKey}`
   );
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
 async function parseData() {
   const data = await getWeather();
-  console.log(data);
   const dataWeather = {
     temp: data.main.temp - 273.15,
     feelsLike: data.main.feels_like,
@@ -31,7 +29,6 @@ async function renderWeather() {
   const description = document.querySelector(".js-description");
 
   const data = await parseData();
-  console.log(data);
 
   temp.insertAdjacentHTML("beforeend", ` ${Math.round(data.temp)} °C`);
   // feelsLike.innerHTML = data.feelsLike;
@@ -41,6 +38,43 @@ async function renderWeather() {
 }
 
 renderWeather();
+
+//get date
+
+function getDate() {
+  const today = new Date();
+  return today;
+}
+
+function renderDate() {
+  const date = document.querySelector(".js-date");
+  const today = getDate();
+  const options = {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+    year: "numeric"
+  };
+  date.innerHTML = today.toLocaleDateString("en-GB", options);
+}
+renderDate();
+
+function renderSeason() {
+  const season = document.getElementById("js-scenery-illustration-icon");
+  const today = getDate();
+  console.log(season);
+  console.log(today.getMonth());
+  if (today.getMonth() >= 2 && today.getMonth() <= 4) {
+    season.src = `./public/seasons/season-spring.png`;
+  } else if (today.getMonth() >= 5 && today.getMonth() <= 7) {
+    season.src = `./public/seasons/season-summer.png`;
+  } else if (today.getMonth() >= 8 && today.getMonth() <= 10) {
+    season.src = `./public/seasons/season-autumn.png`;
+  } else {
+    season.src = `./public/seasons/season-winter.png`;
+  }
+}
+renderSeason();
 
 // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key}
 
